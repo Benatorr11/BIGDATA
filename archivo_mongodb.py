3f6d1108c7949b47f169a229ed54dc3c27b95a1a
Beñat Oribe <61805655+Benatorr11@users.noreply.github.com>
2023-12-04 22:45:33 +0100

    Update archivo_mongodb.py

--- archivo_mongodb.py
+++ archivo_mongodb.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from pymongo import MongoClient
 
-# Conexión a MongoDB (asegúrate de tener el servidor MongoDB en ejecución)
+# Conexión a MongoDB
 client = MongoClient('localhost', 27017)
-db = client['db_practica']  # Reemplaza con el nombre de tu base de datos
+db = client['db_practica'] 
 collection = db['libros']
 
 # Cargar el dataset desde el archivo CSV
-df = pd.read_csv('books.csv', encoding='latin-1', nrows = 100, delimiter = ';')  # Reemplaza con la ruta correcta a tu archivo CSV
+df = pd.read_csv('books.csv', encoding='latin-1', nrows = 100, delimiter = ';')
 
 # Seleccionar los primeros 100 registros del dataset
 subset_df = df.head(100)
